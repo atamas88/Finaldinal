ab9551a0b66c007dab428bc2fa251de67902c37a
Berglind Líf <agnersdottir@gmail.com>
2015-12-14 09:46:23 +0100

    About the View

---
+++ LifeÍnEsbjergClient/LifeInEsbjerg/LifeInEsbjergGateway/Facade.cs
@@ -0,0 +1,36 @@
+﻿using LifeInEsbjergGateway.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeInEsbjergGateway
+{
+   public class Facade
+    {
+        public CompanyGatewayService GetCompanyGateway()
+        {
+            return new CompanyGatewayService();
+        }
+        public CategoryGatewayService GetCategoryGateway()
+        {
+            return new CategoryGatewayService();
+        }
+
+        public TagGatewayService GetTagGateway()
+        {
+            return new TagGatewayService();
+        }
+
+        public RatingGatewayService GetRatingGateway()
+        {
+            return new RatingGatewayService();
+        }
+
+        public ReviewGatewayService GetReviewGateway()
+        {
+            return new ReviewGatewayService();
+        }
+    }
+}
